b9ce6438299aa4538495abf4cf4d27282d16711a
Ondicho <albert.ondicho@moringaschool.com>
2022-06-15 14:33:24 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form=document.querySelector('#create-task-form')
+  form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    addToDoList(e.target.new_task_description.value)
+    form.reset()
+
+  })
+
 });
+
+function addToDoList(todo){
+  let tasks=document.querySelector('#tasks')
+  let li=document.createElement('li')
+  li.innerText=todo
+  tasks.appendChild(li)
+}
